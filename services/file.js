508a737bbd7999cf76c90dea1a733a4a0fd7d73e
yevhent <y.tyshchenko@lexit.co>
2021-11-13 20:51:01 +0200

    add Readme + tiny refactor

--- services/file.js
+++ services/file.js
@@ -6,21 +6,24 @@ async function getFileData(fileId) {
 
   // Check if file info was cached already. If true: return right away.
   const cacheFile = await cache.get(fileId);
-  if (cacheFile) return JSON.parse(cacheFile);
-  
-  files = await getAllFiles();
-  const file = files.filter(file => file.fileId === fileId)[0];
-
-  // const file = {fileId: '0000-0000', processingStatus: 'PROCESSING'};
-
-  if (file && file.processingStatus === 'FINISHED') {   
-    const details = await getFileDetails(fileId);
-    const segments = await getFileSegments(fileId);
-    cache.set(fileId, { ...file, details, segments });
-    
-    return { ...file, details, segments };
+  if (cacheFile) {
+    return JSON.parse(cacheFile);
   } else {
-    throw Error('Exception: requested file info is not ready');
+    files = await getAllFiles();
+    const file = files.filter(file => file.fileId === fileId)[0];
+  
+    // Mock object to test exception response case
+    // const file = {fileId: '0000-0000', processingStatus: 'PROCESSING'};
+
+    if (file && file.processingStatus === 'FINISHED') {   
+      const details = await getFileDetails(fileId);
+      const segments = await getFileSegments(fileId);
+      cache.set(fileId, { ...file, details, segments });
+      
+      return { ...file, details, segments };
+    } else {
+      throw Error('Exception: requested file info is not ready');
+    }
   }
 }
 
